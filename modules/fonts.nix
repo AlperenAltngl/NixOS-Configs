a58ccdf8054bd5a208f4455a50a2df2fa0230814
AlperenAltngl <altingulalperen@gmail.com>
2025-06-02 14:08:38 +0300

    japanese added yippi

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,16 +1,36 @@
-{ ...}: {
-
+{pkgs, ...}: {
   fonts = {
     enableDefaultPackages = true;
+
+    fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      ubuntu_font_family
+      unifont
+      carlito
+      dejavu_fonts
+      ipafont
+      kochi-substitute
+      source-code-pro
+      ttf_bitstream_vera
+    ];
 
     fontconfig = {
       antialias = true;
       defaultFonts = {
-        serif = ["Ubuntu"];
-        sansSerif = ["Ubuntu"];
-        monospace = ["Ubuntu Source"];
+        monospace = [
+          "DejaVu Sans Mono"
+          "IPAGothic"
+        ];
+        sansSerif = [
+          "DejaVu Sans"
+          "IPAPGothic"
+        ];
+        serif = [
+          "DejaVu Serif"
+          "IPAPMincho"
+        ];
       };
     };
   };
-  
 }
